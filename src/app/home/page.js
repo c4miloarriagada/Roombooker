ed8df015add8da3117b3c26be9c818afba19d9b1
Pamela Azúa <pam.azua@duocuc.cl>
2024-09-28 02:42:13 +0900

    Home incompleto
    Falta: menu superior, habitaciones, arreglar fonts.

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,7 +1,23 @@
+import "./home.css";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello Page</h1>
+    <div className="fondo">
+      <h1 className="titulo" >Hotel Pacific Reef</h1>
+        <h1 className="subtitulo" >Encuentra la reserva de tus sueños</h1>
+      <div className="barra">
+          <select>
+            <option value="" disabled selected>Selecciona un tipo de habitación</option>
+            <option value="simple">Habitación Simple</option>
+            <option value="doble">Habitación Doble</option>
+            <option value="suite">Suite</option>
+          </select>
+          <input type="number" placeholder="Número de personas" min="1" />
+          <input type="date" placeholder="Fecha" />
+          <button type="button">Buscar</button>
+      </div>
+      <div className="panel">
+      </div>
     </div>
   )
 }
--- src/app/registro/page.js
+++ src/app/registro/page.js
@@ -1,10 +1,17 @@
 import { FormularioRegistro } from './_component/FormularioRegistro'
 
+import style from './page.module.css'
+
 export default function RegistroPage() {
+
   return (
+
     <div>
-      <h1>Hello Page</h1>
-      <FormularioRegistro />
+
+      <h1 className={style.red}>Hello Page</h1>
+
     </div>
+
   )
+
 }
